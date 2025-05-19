07ed7a0b5670ca1cb58cbbc1c689d42e03ea2afd
Christian Freitas <chrfreitas@gmail.com>
2025-05-19 17:36:54 -0300

    Change user migration

--- db/migrate/20250519202514_create_users.rb
+++ db/migrate/20250519202514_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :email, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_19_202514) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
