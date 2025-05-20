5a71b0747057750ee77e1cf7fc3dc70243238bb5
Christian Freitas <chrfreitas@gmail.com>
2025-05-20 11:46:26 -0300

    Move regex value to a const

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  VALID_PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[[:^alnum:]])[\w[:^alnum:]]{8,}$/
+
   validates :password, format: {
-    with: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[[:^alnum:]])[\w[:^alnum:]]{8,}$/,
+    with: VALID_PASSWORD_REGEX,
     message: 'must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character',
     multiline: true
   }
