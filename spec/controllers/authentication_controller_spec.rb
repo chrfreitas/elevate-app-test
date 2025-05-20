6a540e0dbecb10e86c2bcd0e367a0dfc0e270c90
Christian Freitas <chrfreitas@gmail.com>
2025-05-20 14:57:27 -0300

    Add more test scenarios for authentication controller

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -9,11 +9,18 @@ RSpec.describe Api::AuthenticationController, type: :request do
   end
 
   context "POST /api/sessions" do
-    it "shoudl check if the users exists and return a valid token" do
+    it "should return a valid token for the valid user" do
       create(:user)
 
       post "/api/sessions", params: { email: "user@email.com", password: "Password!123"}
       expect(response).to have_http_status(:ok)
+    end
+
+    it "should return unauthorized status code for the invalida user" do
+      create(:user)
+
+      post "/api/sessions", params: { email: "xxx@email.com", password: "Password!123"}
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
