74382f319aef0f53fe2a794155d6f87c29b4c46e
Christian Freitas <chrfreitas@gmail.com>
2025-05-20 11:53:34 -0300

    Replace hardcoded test for shoulda matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[[:^alnum:]])[\w[:^alnum:]]{8,}$/
 
+  validates :email, presence: true
+
   validates :password, format: {
     with: VALID_PASSWORD_REGEX,
     message: 'must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character',
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'password validation' do
-    it 'must have 1 uppercase, 1 lower case, 1 digit and 1 special character' do
-      user = User.new(email: "blabla", password: "!Password123")
-      expect(user).to be_valid
-    end    
+  context 'validations' do
+    it { should allow_value('!Password123').for(:password) }
   end
 end
