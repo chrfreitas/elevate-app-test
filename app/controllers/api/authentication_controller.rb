ff51aaabf63cec330d8b8c7f48f54c94ecf05a5e
Christian Freitas <chrfreitas@gmail.com>
2025-05-20 10:22:29 -0300

    Create sign in route

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -7,5 +7,17 @@ class Api::AuthenticationController < ApplicationController
       render json: { token: token }, status: :created
     end
   end
+
+
+  def signin
+    user = User.find_by(email: params[:email])
+
+    if user.present?
+      token = Authentication::Token.encode(user_id: user.id)
+      render json: { token: token }, status: :created
+    else
+      render status: :unauthorized
+    end
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    post '/user', to: "authentication#signup"
+    post "/user", to: "authentication#signup"
+    post "/sessions", to: "authentication#signin" 
   end
 end
