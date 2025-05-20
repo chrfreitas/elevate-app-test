2fc55d4b3617d9a7d277165af5058e0e05d6f01a
Christian Freitas <chrfreitas@gmail.com>
2025-05-20 14:39:32 -0300

    Move jwt secret key to docker compose file

--- app/services/authentication/token.rb
+++ app/services/authentication/token.rb
@@ -1,10 +1,8 @@
 require 'jwt'
 
 module Authentication::Token
-  SECRET_KEY = 'blabla'
-
   def self.encode(payload,  exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, ENV["SECRET_KEY"])
   end
 end
