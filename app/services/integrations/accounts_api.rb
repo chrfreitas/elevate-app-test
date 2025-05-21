7117790a7aac3e215f1f927c22f2f1963ed3e072
Christian Freitas <chrfreitas@gmail.com>
2025-05-21 16:10:52 -0300

    Update the contant

--- app/services/integrations/accounts_api.rb
+++ app/services/integrations/accounts_api.rb
@@ -8,7 +8,7 @@ class Integrations::AccountsApi
   def get_billing_info
     url = "#{BASE_URL}/users/#{@user_id}/billing"
     
-    http = Adapters::Http.new(url: url, token: ENV['BILLING_API_KEY'])
+    http = Adapters::Http.new(url: url, token: ENV['ACCOUNT_API_KEY'])
     http.get[:body]
   end
 end
