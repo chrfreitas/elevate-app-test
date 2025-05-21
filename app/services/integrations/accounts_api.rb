303876d87ae648af93d46830a5f3dd23788588d9
Christian Freitas <chrfreitas@gmail.com>
2025-05-21 10:37:33 -0300

    Rename billing to accounts api

--- app/services/integrations/accounts_api.rb
+++ app/services/integrations/accounts_api.rb
@@ -1,20 +1,14 @@
-class Integrations::BillingApi
+class Integrations::AccountsApi
   BASE_URL = "https://interviews-accounts.elevateapp.com/api/v1"
 
   def initialize(user_id)
     @user_id = user_id
   end
 
-  def run 
-    url = billing_url(@user_id)
+  def get_billing_info
+    url = "#{BASE_URL}/users/#{@user_id}/billing"
     
     http = Adapters::Http.new(url: url, token: ENV['BILLING_API_KEY'])
     http.get
-  end
-
-  private 
-
-  def billing_url(user_id)
-    "#{BASE_URL}/users/#{user_id}/billing"
   end
 end
