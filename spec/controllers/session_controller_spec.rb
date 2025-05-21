86aff20db4e5bca6f17513f87bc23a647481a58e
Christian Freitas <chrfreitas@gmail.com>
2025-05-21 17:55:08 -0300

    Create test for the new user controller endpoint

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -2,16 +2,14 @@ require "rails_helper"
 
 RSpec.describe Api::SessionController, type: :request do
   context "POST /api/sessions" do
-    it "should return unauthorized status code for the invalida user" do
-      create(:user)
+    let!(:user) { create(:user) }
 
+    it "should return unauthorized status code for the invalida user" do
       post "/api/sessions", params: { email: "xxx@email.com", password: "Password!123"}
       expect(response).to have_http_status(:unauthorized)
     end
 
     it "should return a valid token for the valid user" do
-      create(:user)
-
       post "/api/sessions", params: { email: "user@email.com", password: "Password!123"}
       expect(response).to have_http_status(:ok)
     end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,42 @@
 require "rails_helper"
 
 RSpec.describe Api::UserController, type: :request do
+  context "GET /api/user/:id" do
+    context "when it didn't find a user" do
+      it "returns not found status code" do
+        get "/api/user/1"
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context "when it finds a user" do
+      let!(:user) {create(:user) }
+
+      it "should return ok status code" do
+        get "/api/user/#{user.id}"
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "should return the user object" do
+        get "/api/user/#{user.id}"
+
+        json = JSON.parse(response.body)
+
+        expect(json).to eq({
+          "user" => {
+            "id" => user.id,
+            "email" => user.email,
+            "stats" => {
+              "total_games_played" => 10
+            },
+            "subscription_status" => nil
+          }
+        })
+
+      end
+    end
+  end
+
   context "POST /api/user" do
     it "should return bad request because the password is incorrect" do
       post "/api/user", params: { email: "test@email.com", password: "12345"}
