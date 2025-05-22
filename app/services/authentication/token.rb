64a9febe7c6265d33130eec870c64a139f263e5a
Christian Freitas <chrfreitas@gmail.com>
2025-05-22 10:46:00 -0300

    Rename secret key to generate tokens

--- app/services/authentication/token.rb
+++ app/services/authentication/token.rb
@@ -3,12 +3,12 @@ require 'jwt'
 module Authentication::Token
   def self.encode(payload,  exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, ENV["SECRET_KEY"])
+    JWT.encode(payload, ENV["TOKEN_SECRET_KEY"])
   end
 
   def self.decode(token)
     return nil if token.nil?
 
-    JWT.decode(token, ENV["SECRET_KEY"]).first
+    JWT.decode(token, ENV["TOKEN_SECRET_KEY"]).first
   end
 end
--- spec/models/game_event_spec.rb
+++ spec/models/game_event_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe GameEvent, type: :model do
   context 'validations' do
-    it {
-      should validate_inclusion_of(:event_type)
-        .in_array(["COMPLETED"])
-    }
+    it { should validate_inclusion_of(:event_type).in_array(["COMPLETED"])}
   end
 end
