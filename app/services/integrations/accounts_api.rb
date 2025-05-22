31610ba822066473348b8e5e0cc2b59b854e021a
Christian Freitas <chrfreitas@gmail.com>
2025-05-22 10:52:57 -0300

    Move URL from account api service to the docker-compose

--- app/services/integrations/accounts_api.rb
+++ app/services/integrations/accounts_api.rb
@@ -1,12 +1,10 @@
 class Integrations::AccountsApi
-  BASE_URL = "https://interviews-accounts.elevateapp.com/api/v1"
-
   def initialize(user_id)
     @user_id = user_id
   end
 
   def get_billing_info
-    url = "#{BASE_URL}/users/#{@user_id}/billing"
+    url = "#{ENV['ACCOUNT_API_URL']}/users/#{@user_id}/billing"
     
     http = Adapters::Http.new(url: url, token: ENV['ACCOUNT_API_KEY'])
     http.get[:body]
