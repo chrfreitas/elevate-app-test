e7e9138cdd6431497e2a77198e7d54ca103d249c
Christian Freitas <chrfreitas@gmail.com>
2025-05-22 10:10:18 -0300

    Fix typo

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -1,7 +1,7 @@
 class GameEvent < ApplicationRecord
   belongs_to :user
 
-  EVEN_TYPES = %w(COMPLETED)
+  EVENT_TYPES = %w(COMPLETED)
 
   validates :event_type, inclusion: { in: EVEN_TYPES, message: "%{value} is not a valid value" }
 end
